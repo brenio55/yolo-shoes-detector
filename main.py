841df5b05c7984aac9b8d84f91a41ba007cb5cc6
Brenio Filho <brenio55@outlook.com>
2025-08-21 19:45:19 -0300

    add: added initial files to detect the shoes and the trained model

---
+++ main.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+model = YOLO("./my_model2/train/weights/best.pt")  # Load a pretrained YOLOv8 model
+
+# model.train(data="data.yaml", epochs=3)  # Train the model on the COCO128 dataset for 3 epochs
+
+results = model.predict(source=0, show=True, conf=0.1)  # Predict on an image and display results
