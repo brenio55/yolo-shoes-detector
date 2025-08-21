30a2d700353a7b87e080c2a308a84f6d3e98a64f
Brenio Filho <brenio55@outlook.com>
2025-08-21 19:48:56 -0300

    add: added counting variable

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ model = YOLO("./my_model2/train/weights/best.pt")  # Load a pretrained YOLOv8 mo
 
 # model.train(data="data.yaml", epochs=3)  # Train the model on the COCO128 dataset for 3 epochs
 
-results = model.predict(source=0, show=True, conf=0.1)  # Predict on an image and display results
+results = model.predict(source=0, show=True, conf=0.1, stream=True)  # Predict on an image and display results
+
+for r in results: #conta os resultados pelo length do objeto
+    count = len(r.boxes)
+    print(f"Objetos detectados: {count}")
